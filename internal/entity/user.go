1448e7b05505906672f4a9759a457c9b668232c4
Maxim <m.kalinchuk@flang.io>
2025-11-23 15:22:17 +0300

    feat(auth) add password hash, generate token func and update migration user db

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -3,7 +3,7 @@ package entity
 import "github.com/google/uuid"
 
 type User struct {
-	ID           uuid.UUID `db:"id"`
+	Id           uuid.UUID `db:"id"`
 	Username     string    `db:"username"`
 	Email        string    `db:"email"`
 	PasswordHash string    `db:"password_hash"`
--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -1,10 +1,18 @@
 package service
 
 import (
-	"fmt"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
+	"github.com/google/uuid"
 	"go.back/internal/dto"
 	"go.back/internal/repository"
+	"golang.org/x/crypto/bcrypt"
+)
+
+const (
+	signInKey = "qrkjk#4#%35FSFJlja#4353KSFjH"
+	tokenTTL  = 24 * time.Hour
 )
 
 type AuthService struct {
@@ -18,6 +26,14 @@ func NewAuthService(repository repository.User) *AuthService {
 }
 
 func (s *AuthService) CreateUser(request dto.Register) (string, error) {
+	passwordHash, err := s.GeneratePasswordHash(request.Password)
+
+	if err != nil {
+		return "", err
+	}
+
+	request.Password = passwordHash
+
 	userId, err := s.repository.CreateUser(request)
 	return userId, err
 }
@@ -26,7 +42,32 @@ func (s *AuthService) GenerateToken(request dto.Login) (string, error) {
 
 	user, err := s.repository.GetUser(request.Email)
 
-	fmt.Print("\n", user, err)
+	if err != nil {
+		return "", err
+	}
 
-	return "", nil
+	type tokenClaims struct {
+		jwt.StandardClaims
+		UserId uuid.UUID `json:"user_id"`
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signInKey))
+}
+
+func (s *AuthService) GeneratePasswordHash(password string) (string, error) {
+	byteHashPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(byteHashPassword), nil
 }
